a6133681e399ca082cd6b5da32b6e01a8eaca22c
merkusvictory <mvhilario23@gmail.com>
2025-02-14 11:25:00 -0500

    lab 3

--- Die.java
+++ Die.java
@@ -1,7 +1,17 @@
 public class Die {
     // attribute
-    private int faceValue = (int)(Math.random() * 6 + 1);;
+    private int faceValue; // encapsulating
     
+    // default constructor - public method w/ no return type
+    public Die() {
+        roll();
+    }
+
+    // constructor with parameters
+    public Die(int num) {
+        faceValue = num;
+    }
+
     // roll method
     public void roll() {
         faceValue = (int)(Math.random() * 6 + 1);
@@ -10,5 +20,16 @@ public class Die {
     // getter method - sole purpose of obtaining a value
     public int getFaceValue() {
         return faceValue;
+    }
+
+    // toString method
+    public String toString() {
+        String result = "Die with face value " + faceValue;
+        return result;
+    }
+
+    // setter method - void method w/ parameters
+    public void setFaceValue(int newFace) {
+        faceValue = newFace;
     }
 }
--- TestDie.java
+++ TestDie.java
@@ -9,7 +9,13 @@ public class TestDie {
 
         System.out.println(die1);
         // print address of die1 in memory
+        System.out.println(die1.getFaceValue()); // default val
         die1.roll();
         System.out.println(die1.getFaceValue());
+
+        Die die2 = new Die(4);
+        System.out.println(die2.getFaceValue());
+
+        System.out.println(die1 + "\n" + die2.toString());
     }
 }
